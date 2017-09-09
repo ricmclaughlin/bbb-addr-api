8055abbc7c35cb1aa4ef99e3a20f30842c66c5b1
ricmclaughlin <ric@mclaughlin.today>
2017-09-09 12:42:17 -0700

    time

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,12 @@
 from __future__ import print_function
 
 import json
-
+import arrow
 print('Loading function')
 
 def lambda_handler(event, context):
+  utc = arrow.utcnow()
+  local = utc.to('US/Pacific')
   # print("Received event: " + json.dumps(event, indent=2))
   # print("value1 = " + event['key1'])
   # print("value2 = " + event['key2'])
@@ -12,7 +14,7 @@ def lambda_handler(event, context):
   # return event['key1']  # Echo back the first key value
   # message = 'Hello {} {}!'.format(event['first_name'], 
   #                                 event['last_name'])  
-  return response(200, event['queryStringParameters']['first_name'])
+  return response(200, local.format('YYYY-MM-DD HH:mm:ss ZZ'))
 
 def response(status_code, response_body=None):
   return {
